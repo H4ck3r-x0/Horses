caa7c7300155b48dd2af4357d88bfa51e89f2998
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2016-12-29 08:37:30 +0000

    create the index function to show all ads.

--- app/Http/Controllers/Ads/AdsController.php
+++ app/Http/Controllers/Ads/AdsController.php
@@ -7,5 +7,13 @@ use App\Http\Controllers\Controller;
 
 class AdsController extends Controller
 {
-    //
+  /**
+   * Show all ads.
+   *
+   * @return \Illuminate\Http\Response
+   */
+    public function index()
+    {
+      return view('ads.index');
+    }
 }
