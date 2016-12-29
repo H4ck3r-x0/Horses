a681afb44075c48e1df924a6d3ef2e13e0c83893
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2016-12-29 09:25:19 +0000

    create  view for the index function on adscontroller

---
+++ resources/views/Ads/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          <h1>All Ads</h1>
+          <p class="text-muted">You can watch and view all the ads.</p>
+        </div>
+    </div>
+</div>
+@endsection
