37e553c497e72ab6d66f61338dffbe0abc9de928
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2016-12-29 09:43:11 +0000

    add create function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,14 @@ class HomeController extends Controller
     {
         return view('home.home');
     }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+     public function create()
+     {
+       return view('Ads.create');
+     }
 }
