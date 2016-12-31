a2f551a8399336e80e86748c21acf946b046cacf
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2016-12-31 14:57:31 +0000

    remove field

--- database/migrations/2016_12_29_080536_create_ads_table.php
+++ database/migrations/2016_12_29_080536_create_ads_table.php
@@ -18,7 +18,6 @@ class CreateAdsTable extends Migration
             $table->string('title');
             $table->text('description');
             $table->string('location');
-            $table->string('thumbnail');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')
                   ->references('id')->on('users')
