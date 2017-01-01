bf16a3764c85c40231b2a89c75ee21c0319204ed
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2017-01-01 11:42:44 +0000

    edit form action

--- resources/views/Ads/create.blade.php
+++ resources/views/Ads/create.blade.php
@@ -7,7 +7,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Create an Advertisement</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('AdStore') }}">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('storeAd') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
