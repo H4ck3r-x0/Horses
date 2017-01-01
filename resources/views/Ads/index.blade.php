f81671f5117f115b2006b5246f8924fc3e973a7c
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2017-01-01 11:42:59 +0000

    edit layout

--- resources/views/Ads/index.blade.php
+++ resources/views/Ads/index.blade.php
@@ -3,9 +3,30 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
           <h1>All Ads</h1>
-          <p class="text-muted">You can watch and view all the ads.</p>
+          <p class="text-muted">You can watch and view all the advertisements.</p>
+          @foreach ($ads as $ad)
+            <div class="media">
+              <div class="media-left media-middle">
+                @foreach ($ad->adMedia as $media)
+                  @foreach (unserialize($media->media) as $image)
+
+                    <a href="#">
+                      <img class="media-object" src="{{ asset($image) }}" alt="...">
+                    </a>
+                  @endforeach
+                @endforeach
+
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">{{ $ad->title }}</h4>
+                {{ $ad->description }}
+              </div>
+            </div>
+
+          @endforeach
+
         </div>
     </div>
 </div>
