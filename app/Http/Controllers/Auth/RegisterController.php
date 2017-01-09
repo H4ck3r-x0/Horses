38753b1090cbd074edd1d6ec7f3d15a427174f7a
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2017-01-09 15:45:56 +0000

    change redirect to

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = 'ads/all';
 
     /**
      * Create a new controller instance.
