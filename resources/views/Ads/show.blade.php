83209f1ecc9e0c1f705a871b8c22e0a03ceb154f
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2017-01-20 14:38:02 +0000

    layout

---
+++ resources/views/Ads/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-7">
+      <div class="panel panel-default">
+        <div class="panel-heading">{{ $ad->user->username }}</div>
+        <div class="panel-body">
+          <h3>{{ $ad->title }}</h3>
+          {{ $ad->description }}
+        </div>
+      </div>
+    </div>
+    <div class="col-md-5">
+      <div class="panel panel-default">
+        <div class="panel-heading">Advertisements Media</div>
+        <div class="panel-body">
+        </div>
+        @include('Ads.partials.mediaSlide')
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
